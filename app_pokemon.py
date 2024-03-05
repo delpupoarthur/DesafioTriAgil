9397494a7b04e5a1e59b7ee1fe6f883278808853
delpupoarthur <arthurdelpupocoelho40@gmail.com>
2024-03-04 21:56:26 -0300

    Segundo commit

--- app_pokemon.py
+++ app_pokemon.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 import requests
 import json
+import os
 
 app = Flask(__name__)
 
@@ -17,28 +18,34 @@ def get_pokemon_data(pokemon_name):
     else:
         return None, None, None
 
+# Função para criar um novo ID para o time
+def get_next_team_id(teams):
+    return str(len(teams) + 1)
+
 # Rota para listar todos os times registrados
 @app.route('/api/teams', methods=['GET'])
 def get_teams():
     try:
         with open('teams.txt', 'r') as file:
             teams = json.load(file)
+            if not teams:
+                return jsonify({'message': 'Nenhum time Pokemon cadastrado'}), 404
             return jsonify(teams)
-    except FileNotFoundError:
-        return jsonify({'error': 'Nenhum time registrado'}), 404
+    except (FileNotFoundError, json.decoder.JSONDecodeError):
+        return jsonify({'message': 'Nenhum time Pokemon cadastrado'}), 404
 
-# Rota para buscar um time registrado por usuário
-@app.route('/api/teams/<string:user>', methods=['GET'])
-def get_team_by_user(user):
+# Rota para buscar um time registrado por ID
+@app.route('/api/teams/<int:team_id>', methods=['GET'])
+def get_team_by_id(team_id):
     try:
         with open('teams.txt', 'r') as file:
             teams = json.load(file)
-            team = teams.get(user)
+            team = teams.get(str(team_id))
             if team:
                 return jsonify(team)
             else:
                 return jsonify({'error': 'Time não encontrado'}), 404
-    except FileNotFoundError:
+    except (FileNotFoundError, json.decoder.JSONDecodeError):
         return jsonify({'error': 'Nenhum time registrado'}), 404
 
 # Rota para criação de um time de Pokémon
@@ -66,15 +73,21 @@ def create_team():
     try:
         with open('teams.txt', 'r') as file:
             teams = json.load(file)
-    except FileNotFoundError:
+    except (FileNotFoundError, json.decoder.JSONDecodeError):
         teams = {}
 
-    teams[user] = team_data
+    team_id = get_next_team_id(teams)
+    new_team = {'owner': user, 'pokemons': team_data}
+
+    teams[team_id] = new_team
 
     with open('teams.txt', 'w') as file:
         json.dump(teams, file)
 
-    return jsonify({'message': 'Time criado com sucesso', 'id': len(teams)}), 201
+    return jsonify({'message': 'Time criado com sucesso', 'id': team_id}), 201
 
 if __name__ == '__main__':
+    if not os.path.isfile('teams.txt'):
+        with open('teams.txt', 'w') as file:
+            file.write('{}')
     app.run(debug=True, host='0.0.0.0')
